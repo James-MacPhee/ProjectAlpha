49e617b6ecd92bb7b719ff451b7e7fe458c11f3e
Matt Peachey <matthew_peachey@hotmail.com>
2018-12-19 21:13:21 -0400

    Added PoseNet to sketch.js!

--- sketch.js
+++ sketch.js
@@ -1,5 +1,17 @@
+let video;
+let poseNet;
+let count = 0;
+
 function setup(){
     var canvas = createCanvas(1200,650);
     canvas.parent('canvasArea');
-    background(50);
+    video = createCapture(VIDEO);
+    video.size(1200,650);
+    video.hide();
+    poseNet = ml5.poseNet(video,"single"); // This is the posenet object
+
+}
+
+function draw(){
+    image(video,0,0,1200,650);
 }
